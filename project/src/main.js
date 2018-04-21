bb99f453975b7bfdf37f4c1956266b4ed8c59c28
FangFeiyue <18766131540@163.com>
2018-04-21 22:49:58 +0800

    移动端1像素问题和点击延迟300毫秒问题

--- project/src/main.js
+++ project/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
