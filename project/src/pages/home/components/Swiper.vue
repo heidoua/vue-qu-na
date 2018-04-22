80ed3e98c681da805d1ac058974d40f1d77308c0
FangFeiyue <18766131540@163.com>
2018-04-22 10:11:39 +0800

    引入vue-awesome-swiper

---
+++ project/src/pages/home/components/Swiper.vue
@@ -0,0 +1,32 @@
+<template>
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+        <!-- slides -->
+        <swiper-slide>I'm Slide 1</swiper-slide>
+        <swiper-slide>I'm Slide 2</swiper-slide>
+        <swiper-slide>I'm Slide 3</swiper-slide>
+        <swiper-slide>I'm Slide 4</swiper-slide>
+        <swiper-slide>I'm Slide 5</swiper-slide>
+        <swiper-slide>I'm Slide 6</swiper-slide>
+        <swiper-slide>I'm Slide 7</swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-scrollbar"   slot="scrollbar"></div>
+    </swiper>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data: function () { 
+        return {
+            
+        };
+     }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
