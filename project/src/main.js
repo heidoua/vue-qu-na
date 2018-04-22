2eb11bdc8757982d4d73a8bb1688feacd4e77896
FangFeiyue <18766131540@163.com>
2018-04-22 09:49:50 +0800

    给styles起别名

--- project/src/main.js
+++ project/src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
--- project/src/pages/home/components/Header.vue
+++ project/src/pages/home/components/Header.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    @import '../../../assets/styles/varibles.styl';
+    @import '~styles/varibles.styl';
     .header
         display: flex
         line-height: .86rem
