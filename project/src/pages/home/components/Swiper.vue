e89fde16d7d7fe6b0ef9e4c8ce818975c5b19299
FangFeiyue <18766131540@163.com>
2018-04-22 12:25:59 +0800

    轮播图调整

--- project/src/pages/home/components/Swiper.vue
+++ project/src/pages/home/components/Swiper.vue
@@ -2,20 +2,8 @@
     <div class="wrapper">
         <swiper :options="swiperOption">
         <!-- slides -->
-            <swiper-slide>
-                <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_640x200_4c7220d4.jpg" alt="">
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img"
-                src="http://img1.qunarzz.com/piao/fusion/1803/98/0a2e0bd8eb0c0802.png_640x200_0aae3331.png" alt="">
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img"
-                src="http://img1.qunarzz.com/piao/fusion/1804/1b/6c766b79e2045102.jpg_640x200_3e44c74a.jpg" alt="">
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img"
-                src="http://img1.qunarzz.com/piao/fusion/1804/ab/2d291e6cf2bc7f02.jpg_640x200_fa5f0167.jpg" alt="">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="">
             </swiper-slide>
             <!-- Optional controls -->
             <div class="swiper-pagination"  slot="pagination"></div>
@@ -29,8 +17,27 @@ export default {
   data: () => {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
-      }
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_640x200_4c7220d4.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/98/0a2e0bd8eb0c0802.png_640x200_0aae3331.png'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/1b/6c766b79e2045102.jpg_640x200_3e44c74a.jpg'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ab/2d291e6cf2bc7f02.jpg_640x200_fa5f0167.jpg'
+        }
+      ]
     }
   }
 }
