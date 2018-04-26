42a69d82ec8b11cc53ba898872a78ae8a7692046
FangFeiyue <18766131540@163.com>
2018-04-26 15:02:35 +0800

    城市选择页面输入框布局

---
+++ project/src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .search
+        height 0.72rem
+        padding 0 .1rem
+        background-color $bgColor
+        .search-input
+            box-sizing border-box
+            width 100%
+            height 0.62rem
+            line-height .62rem
+            text-align center
+            border-radius .06rem
+            color #666666
+            padding 0 .1rem
+
+</style>
