c553c5beca1cdd19c5445c250fb1ad5785c7b335
FangFeiyue <18766131540@163.com>
2018-05-01 22:24:45 +0800

    search-content样式调整

--- project/src/pages/city/components/Search.vue
+++ project/src/pages/city/components/Search.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input class="search-input" type="text" placeholder="输入城市名或拼音">
+        </div>
+        <div class="search-content">
+            <ul>
+                <li>hello world</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -25,5 +32,13 @@ export default {
             border-radius .06rem
             color #666666
             padding 0 .1rem
-
+    .search-content
+        z-index 1
+        overflow hidden
+        position absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        background-color green
 </style>
