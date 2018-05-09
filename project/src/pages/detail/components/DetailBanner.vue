eab912b08597d448bc0d91b333ecf49f96fabd22
FangFeiyue <18766131540@163.com>
2018-05-09 14:04:19 +0800

    开始制作详情页

---
+++ project/src/pages/detail/components/DetailBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- project/src/pages/home/components/Recommend.vue
+++ project/src/pages/home/components/Recommend.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="recommend-title">热销推荐</div>
     <ul>
-        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <router-link tag="li" class="item border-bottom" v-for="item of recommendList" :key="item.id" :to="'/detial/'+item.id">
             <img class="item-img" :src="item.imgUrl" alt="">
             <div class="item-info">
                 <p class="item-title">{{item.title}}</p>
                 <p class="item-desc">{{item.desc}}</p>
                 <button class="item-button">查看详情</button>
             </div>
-        </li>
+        </router-link>
     </ul>
   </div>
 </template>
